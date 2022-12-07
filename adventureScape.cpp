c6d05495d37a34fcc8588ad3a27dc769209306dc
savannah7704 <savannah7704@comcast.net>
2022-12-07 10:26:58 -0600

    Update files.

--- adventureScape.cpp
+++ adventureScape.cpp
@@ -8,21 +8,17 @@ int main()
 
     string playerAnswer;
 
-    cout << "Welcome to Adventure Scape. This is an interactive text based adventure game. Along your journey, you will be presented with different choices that will take
-    the story on different paths. Simply type in your choice and press enter in order to continue your adventure. Have fun! ";
-    
+    cout << "Welcome to Adventure Scape. This is an interactive text based adventure game. Along your journey, you will be presented with different choices that will takethe story on different paths. Simply type in your choice and press enter in order to continue your adventure. Have fun! ";
     cout << "PRESS ENTER WHEN YOU ARE READY TO BEGIN.";
     getline(cin);
     
     cout << "You are a skilled archaeologist and explorer. You are on a quest to find a mythical ancient Egyptian artifact called the Tablet of Spellbinding. ";
     cout << "According to legend, the engravings on the tablet are said to tell the secret to obtaining magical powers. ";
-    cout << "The mere thought of acquiring such an item entices you. You eagerly embark on your journey, closely grasping a pendant you acquired from a previous joruney that
-    is said to glow when it is close to the ancient tomb containing the tablet. ";
-    cout << "You After trekking through a ferocious sandstorm, you arrive at a location that makes your pendant shine brightly. Suddenly, a beam of light shoots out of the
-    pendant. You follow the light and arrive at an abandoned tomb. You take a deep breath and head inside. ";
+    cout << "The mere thought of acquiring such an item entices you. You eagerly embark on your journey, closely grasping a pendant you acquired from a previous journey that is said to glow when it is close to the ancient tomb containing the tablet. ";
+    cout << "You After trekking through a ferocious sandstorm, you arrive at a location that makes your pendant shine brightly. Suddenly, a beam of light shoots out of the pendant. You follow the light and arrive at an abandoned tomb. You take a deep breath and head inside. ";
     cout << "PRESS ENTER.";
     getline(cin);
 
-    
+
 }
 
