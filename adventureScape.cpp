1c15ba2a8e66486d628ac16f5658b65da41adda7
savannah7704 <savannah7704@comcast.net>
2022-12-07 10:09:36 -0600

    Update files.

--- adventureScape.cpp
+++ adventureScape.cpp
@@ -7,6 +7,11 @@ int main()
 {
 
     string playerAnswer;
-
+    
+    cout << "You are a skilled archaeologist and explorer. You are on a quest to find a mythical ancient Egyptian artifact called the Tablet of Spellbinding. ";
+    cout << "According to legend, the engravings on the tablet are said to tell the secret to obtaining magical powers. ";
+    cout << "The mere thought of acquiring such an item entices you. You eagerly embark on your journey, closely grasping the pendent around your neck that is said to 
+    reveal the location of the ancient tomb containing the Tablet of Spellbinding. ";
+    cout << "After trekking through a ferocious sandstorm, you arrive at a location that makes your pendant shine brightly. "
 }
 
